d95587101a8de6afaebdbbc4008a48d35a904555
Zakariya Sattar <zakariyasattar03@gmail.com>
2021-02-02 18:00:23 -0600

    got basic sound and pause and play to work

--- files/index.js
+++ files/index.js
@@ -1,4 +1,12 @@
 window.onload = function(){
+
+  document.onkeypress = function (e) {
+    e = e || window.event;
+    if(e.keyCode == 13 || e.keyCode == 32) {
+      togglePlayPause();
+    }
+  };
+
   var name = document.getElementById('name');
 
   if(name.offsetWidth < name.scrollWidth) {
@@ -11,20 +19,38 @@ window.onload = function(){
     });
   }
 
-  determineVolumeIcon();
-  new Audio('C:\Users\Zakariya Sattar\Downloads\MediaHuman\Music\Sosa Montana · Lil Uzi - Srt!').play()
+  $("#volume").hover(function(){
+    console.log("hello")
+      document.getElementById('vol-control').style.display = "";
+    }, function(){
+      document.getElementById('vol-control').style.display = "none";
+  });
 
-function togglePlayPause(x) {
-  x.style.display = "none";
+};
+
+var song = new Audio('/Users/zakariyasattar/Music/iTunes/iTunes Media/Music/Playboi Carti/Unknown Album/Pop Up (Remastered) [BEST VERSION].mp3');
+
+function togglePlayPause() {
+  var x;
+  if(document.getElementById('pause-button').style.display != "none") {
+    x = document.getElementById('pause-button');
+  }
+  else {
+    x = document.getElementById('play-button');
+  }
 
   if(x.className == "fa fa-play") {
     document.getElementById('pause-button').style.display = "";
+    document.getElementById('play-button').style.display = "none";
+    song.play();
   }
   else {
     document.getElementById('play-button').style.display = "";
+    document.getElementById('pause-button').style.display = "none";
+    song.pause();
   }
 }
 
-function determineVolumeIcon() {
-
+function setVolume(volume) {
+  song.volume = volume / 100;
 }
